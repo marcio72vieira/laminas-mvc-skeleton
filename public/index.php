bc6ed19caffe689d5aefb88a15a440f69fa4e1f2
marcio72vieira <marcio72vieira@hotmail.com>
2022-06-03 14:08:49 -0300

    Set display erros in developtment

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ if ($_SERVER['APPLICATION_ENV'] === 'development') {
     ini_set("display_errors", '1');
 }
 
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
